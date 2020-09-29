a59c65e9d3f77d8a725b96f47343f5cdf1dfd72f
Feisal Mohamud Ahmed <6140236@RTS-365.dk>
2020-09-29 10:37:58 +0200

    asset changes in api

--- controllers/asset.controller.js
+++ controllers/asset.controller.js
@@ -2,40 +2,54 @@ var { Asset } = require("../models/models");
 var saveFile = require("../services/asset");
 
 async function createSingleAsset(req, res, next) {
-	try {
-		let file = saveFile(req.files.file);
-		let asset = await Asset.create({
-			url: "http://localhost:4000/file-bucket/" + file
-		});
-		res.json(asset);
-	} catch (error) {
-		console.error(error);
-		res.status(500).end();
-	}
+  try {
+    let file = saveFile(req.files.file);
+    let asset = await Asset.create({
+      url: "https://svendeproeveapi.herokuapp.com/file-bucket/" + file,
+    });
+    res.json(asset);
+  } catch (error) {
+    console.error(error);
+    res.status(500).end();
+  }
 }
 
 async function getAllAssets(req, res, next) {
-	try {
-		let assets = await Asset.findAll();
-		res.json(assets);
-	} catch (error) {
-		console.error(error);
-		res.status(500).end();
-	}
+  try {
+    let assets = await Asset.findAll();
+    res.json(assets);
+  } catch (error) {
+    console.error(error);
+    res.status(500).end();
+  }
 }
 
 async function getSingleAsset(req, res, next) {
-	try {
-		let asset = await Asset.findByPk(req.params.id);
-		res.json(asset);
-	} catch (error) {
-		console.error(error);
-		res.status(500).end();
-	}
+  try {
+    let asset = await Asset.findByPk(req.params.id);
+    res.json(asset);
+  } catch (error) {
+    console.error(error);
+    res.status(500).end();
+  }
+}
+
+async function updateSingleAsset(req, res, next) {
+  try {
+    let asset = await Asset.findByPk(req.params.id);
+    asset.update({
+      url: req.fields.url,
+    });
+    res.json(asset);
+  } catch (error) {
+    console.error(error);
+    res.status(500).end();
+  }
 }
 
 module.exports = {
-	createSingleAsset,
-	getAllAssets,
-	getSingleAsset
+  createSingleAsset,
+  getAllAssets,
+  getSingleAsset,
+  updateSingleAsset,
 };
